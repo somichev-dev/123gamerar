75cdbf93b6603099c970c1a030135a53ec7d298e
Tulleny <maximazhnakin@yandex.ru>
2026-08-15 01:05:27 +0300

    Testin Godot

--- node_2d.gd
+++ node_2d.gd
@@ -8,16 +8,25 @@ func _ready() -> void:
 		var my_random_number = rng.randi_range(1, 100)
 		if my_random_number % 3 == 0:
 			print(my_random_number)
-
+	$Timer.start()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Sprite2D.rotation_degrees += 360*delta
+	$Sprite2D.rotation_degrees += 360*delta if mouse else -360*delta
 	pass
 
-
+var mouse
 func _on_area_2d_mouse_entered() -> void:
-	print("TEXT")
+	mouse = true
+	pass # Replace with function body.
+
+func _on_area_2d_mouse_exited() -> void:
+	mouse = false
+	pass # Replace with function body.
+
+
+func _on_timer_timeout() -> void:
+	print('еуые')
 	pass # Replace with function body.
