102f29cd1848040322be693e4a50a2e5f452c3ba
Tulleny <maximazhnakin@yandex.ru>
2026-08-15 20:56:12 +0300

    Ачивки (#3)
    Co-authored-by: somichev <84933205+somichev-dev@users.noreply.github.com>

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+func _ready() -> void:
+	Achievements.unlock("start_game") 
+	print(Achievements.achievements)
+
 func _process(delta: float) -> void:
 	$Sprite2D.rotation_degrees += 360*delta if mouse else -360*delta
 
