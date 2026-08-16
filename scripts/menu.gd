fea30c341ab96322c1cd2a8e3e42775abb00aa60
Tulleny <maximazhnakin@yandex.ru>
2026-08-16 16:21:31 +0300

    Починить меню (#13)

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,4 +6,4 @@ func _on_button_button_down() -> void:
 
 
 func _on_stick_anim_player_animation_finished(anim_name: StringName) -> void:
-	get_tree().change_scene_to_file(LevelSelector.get_next_level())
+	get_tree().change_scene_to_file("res://scenes/transition_screen.tscn")
