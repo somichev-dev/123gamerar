8c9f134d75baabe2972db01dd1d900aceafa67cc
Tulleny <maximazhnakin@yandex.ru>
2026-08-16 15:02:26 +0300

    Spell casting (#11)
    Мини-игра по спелл-кастингу
    
    ---------
    
    Co-authored-by: somichev-dev <84933205+somichev-dev@users.noreply.github.com>

--- scripts/level_selector.gd
+++ scripts/level_selector.gd
@@ -3,20 +3,17 @@ extends Node
 @export var level_list = [
 	"res://scenes/rock_bonfire.tscn",
 	"res://scenes/stick_bonfire.tscn",
+	"res://scenes/spell_casting.tscn",
 ]
 @onready var _current_levels = []
 
 
 func get_next_level() -> String:
 	if _current_levels.is_empty():
-		_regen_levels()
+		reset_levels()
 	return _current_levels.pop_at(randi_range(0, _current_levels.size() - 1))
 
 
 func reset_levels() -> void:
-	_regen_levels()
-
-
-func _regen_levels() -> void:
 	_current_levels = level_list.duplicate()
 	_current_levels.shuffle()
