353d2fcd29ab741280fe0de722d28e92b6634f92
somichev-dev <84933205+somichev-dev@users.noreply.github.com>
2026-08-16 20:09:05 +0300

    Передвинуть файлы

--- scripts/level_selector.gd
+++ scripts/level_selector.gd
@@ -1,10 +1,10 @@
 extends Node
 
 @export var level_list = [
-	"res://scenes/rock_bonfire.tscn",
-	"res://scenes/stick_bonfire.tscn",
-	"res://scenes/do_nothing.tscn",
-	"res://scenes/spell_casting.tscn",
+	"res://scenes/games/rock_bonfire.tscn",
+	"res://scenes/games/stick_bonfire.tscn",
+	"res://scenes/games/do_nothing.tscn",
+	"res://scenes/games/spell_casting.tscn",
 ]
 @onready var _current_levels = []
 
